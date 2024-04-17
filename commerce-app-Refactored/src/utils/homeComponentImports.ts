601a2b68c5c8cce3addf2ecfec2b2886f145a978
Yigit <142043726+yeeegit@users.noreply.github.com>
2024-04-17 18:36:31 +0300

    Delete commerce-app-Refactored directory

--- commerce-app-Refactored/src/utils/homeComponentImports.ts
+++
@@ -1,7 +0,0 @@
-export { default as Navbar } from "../components/home/Navbar";
-export { default as SubCategoriesBar } from "../components/home/SubCategories";
-export { default as MainPage } from "../components/home/MainPage";
-export { default as CategoryCards } from "../components/home/CategoryCards";
-export { default as About } from "../components/home/About";
-export { default as Footer } from "../components/home/Footer";
-export { default as Cart } from "../pages/Cart";
